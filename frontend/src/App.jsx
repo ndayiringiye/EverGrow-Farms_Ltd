0bc04aa1519c94a6c80fa3c77657ae66b1f9cce7
ndayiringiye <ndayringiyedavid120@gmail.com>
2025-10-21 10:35:28 +0200

    evergrow farms ltd project set up on both frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+ <div>hello world, my name is david and I' am 23 old aged thank</div>
+    </div>
+  )
+}
+
+export default App
